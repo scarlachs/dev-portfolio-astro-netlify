12583f9a58662903a8987de9460ddbe219fb492d
Pascal <butler.ergot_0s@icloud.com>
2024-09-10 00:28:40 +0200

    add unique id

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,6 +1,27 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
-}
+};
+
+const randomAlphaNumeric = (length: number) => {
+	// let s = "";
+	// Array.from({ length }).some(() => {
+	// 	s += Math.random().toString(36).slice(2);
+	// 	return s.length >= length;
+	// });
+	// return s.slice(0, length);
+
+	const chars =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+
+	Array.from({ length }).forEach(() => {
+		result += chars[Math.floor(Math.random() * chars.length)];
+	});
+
+	return result;
+};
+
+export { cn, randomAlphaNumeric };
