16671f4a5229fbd15d62e0c53771dbd80081b369
Pascal <butler.ergot_0s@icloud.com>
2024-09-09 22:34:35 +0200

    fix indents

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      tailwind({
-          applyBaseStyles: false,
-      }),
-      react(),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
 	],
 
-  output: "server",
-  adapter: netlify(),
+	output: "server",
+	adapter: netlify(),
 });
